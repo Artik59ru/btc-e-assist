0de0bb7bc5a2473f06f3056b1baa716abfe52de9
Alexander Sjn <alexandersjn@gmail.com>
2014-11-05 15:23:27 +0300

    Add AssistSynchronize

---
+++ app/src/main/java/com/btc_e_assist/AssistSynchronize.java
@@ -0,0 +1,17 @@
+package com.btc_e_assist;
+
+public class AssistSynchronize {
+    public static void synchronizedWithDB() {
+
+    }
+
+    private static void synchronizeSimpleAlarmTask() {
+
+    }
+
+    private static void synchronizeOrderAlarmTask() {
+
+    }
+
+}
+
