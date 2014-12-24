5f88cbee276487c9d78cc36dc2ba1ced77b80cb4
Alexander Sjn <alexandersjn@gmail.com>
2014-12-24 13:49:24 +0300

    Little code optimization

--- app/src/main/java/com/btc_e_assist/BalanceFragment.java
+++ app/src/main/java/com/btc_e_assist/BalanceFragment.java
@@ -1,8 +1,5 @@
 package com.btc_e_assist;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Resources;
@@ -16,152 +13,154 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class BalanceFragment extends ListFragment {
-	private Context mContext;
-	private TradeControl tradeControl;
-	private CustomAdapter adapter;
-	private String currentFragmentName = "";
-	private static volatile DataBox dataBox = new DataBox();
-	private SecondThread secondThread;
+    private Context mContext;
+    private TradeControl tradeControl;
+    private CustomAdapter adapter;
+    private String currentFragmentName = "";
+    private static volatile DataBox dataBox = new DataBox();
+    private SecondThread secondThread;
 
-	@Override
-	public void onAttach(Activity activity) {
-		super.onAttach(activity);
-		mContext = activity;
-		currentFragmentName = getTag();
-		tradeControl = TradeControl.getInstance();
-		CommonHelper.showPasswordDialog(mContext);
-		update();
-	}
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mContext = activity;
+        currentFragmentName = getTag();
+        tradeControl = TradeControl.getInstance();
+        CommonHelper.showPasswordDialog(mContext);
+        update();
+    }
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		setHasOptionsMenu(true);
-		View rootView = inflater.inflate(R.layout.balance_list_layout, null);
-		String[] from = { "name", "value" };
-		int[] to = { R.id.itemBalanceName, R.id.itemBalanceValue };
-		adapter = new CustomAdapter(mContext, dataBox.data1,
-				R.layout.item_balance_fragment, from, to);
-		setListAdapter(adapter);
-		return rootView;
-	}
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        View rootView = inflater.inflate(R.layout.balance_list_layout, null);
+        String[] from = {"name", "value"};
+        int[] to = {R.id.itemBalanceName, R.id.itemBalanceValue};
+        adapter = new CustomAdapter(mContext, dataBox.data1,
+                R.layout.item_balance_fragment, from, to);
+        setListAdapter(adapter);
+        return rootView;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		switch (item.getItemId()) {
-		case R.id.action_refresh:
-			update();
-			return true;
-		default:
-			return super.onOptionsItemSelected(item);
-		}
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                update();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
-	private void update() {
-		if (!tradeControl.tradeApi.isKeysInstalled()) {
-			CommonHelper.makeToastNoKeys(mContext);
-		} else {
-			if (secondThread != null) {
-				secondThread.cancel(false);
-			}
-			secondThread = new SecondThread();
-			secondThread.execute();
-			CommonHelper.makeToastUpdating(mContext, currentFragmentName);
-		}
-	}
+    private void update() {
+        if (!tradeControl.tradeApi.isKeysInstalled()) {
+            CommonHelper.makeToastNoKeys(mContext);
+        } else {
+            if (secondThread != null) {
+                secondThread.cancel(false);
+            }
+            secondThread = new SecondThread();
+            secondThread.execute();
+            CommonHelper.makeToastUpdating(mContext, currentFragmentName);
+        }
+    }
 
-	private class SecondThread extends AsyncTask<Void, Void, Boolean> {
+    private class SecondThread extends AsyncTask<Void, Void, Boolean> {
 
-		@Override
-		protected Boolean doInBackground(Void... arg0) {
-			return tradeControl.loadBalanceData();
-		}
+        @Override
+        protected Boolean doInBackground(Void... arg0) {
+            return tradeControl.loadBalanceData();
+        }
 
-		@Override
-		protected void onPostExecute(Boolean result) {
-			super.onPostExecute(result);
-			if (!isAdded()) {
-				return;
-			}
-			if (result.booleanValue() && tradeControl.setBalanceData(dataBox)) {
-				if (adapter != null) {
-					adapter.notifyDataSetChanged();
-					CommonHelper
-							.makeToastUpdated(mContext, currentFragmentName);
-				}
-			} else {
-				CommonHelper.makeToastErrorConnection(mContext,
-						currentFragmentName);
-			}
-		}
-	}
+        @Override
+        protected void onPostExecute(Boolean result) {
+            super.onPostExecute(result);
+            if (!isAdded()) {
+                return;
+            }
+            if (result.booleanValue() && tradeControl.setBalanceData(dataBox)) {
+                if (adapter != null) {
+                    adapter.notifyDataSetChanged();
+                    CommonHelper.makeToastUpdated(mContext, currentFragmentName);
+                }
+            } else {
+                CommonHelper.makeToastErrorConnection(mContext,
+                        currentFragmentName);
+            }
+        }
+    }
 
-	private class CustomAdapter extends BaseAdapter {
-		private LayoutInflater mInflater;
-		private ArrayList<HashMap<String, Object>> mData;
-		private int mLayoutId;
-		private String[] mFrom;
-		private int[] mTo;
-		private int firstColor;
-		private int secondColor;
+    private class CustomAdapter extends BaseAdapter {
+        private LayoutInflater mInflater;
+        private ArrayList<HashMap<String, Object>> mData;
+        private int mLayoutId;
+        private String[] mFrom;
+        private int[] mTo;
+        private int firstColor;
+        private int secondColor;
 
-		CustomAdapter(Context context, ArrayList<HashMap<String, Object>> data,
-				int id, String[] from, int[] to) {
-			mInflater = (LayoutInflater) context
-					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-			mData = data;
-			mLayoutId = id;
-			mFrom = from;
-			mTo = to;
-			Resources res = context.getResources();
-			firstColor = res.getColor(R.color.Black70);
-			secondColor = res.getColor(R.color.Red2);
-		}
+        CustomAdapter(Context context, ArrayList<HashMap<String, Object>> data,
+                      int id, String[] from, int[] to) {
+            mInflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            mData = data;
+            mLayoutId = id;
+            mFrom = from;
+            mTo = to;
+            Resources res = context.getResources();
+            firstColor = res.getColor(R.color.Black70);
+            secondColor = res.getColor(R.color.Red2);
+        }
 
-		@Override
-		public int getCount() {
-			return mData.size();
-		}
+        @Override
+        public int getCount() {
+            return mData.size();
+        }
 
-		@Override
-		public Object getItem(int position) {
-			return mData.get(position);
-		}
+        @Override
+        public Object getItem(int position) {
+            return mData.get(position);
+        }
 
-		@Override
-		public long getItemId(int position) {
-			return position;
-		}
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
 
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			View view = convertView;
-			HashMap<String, Object> map = mData.get(position);
-			if (view == null) {
-				view = mInflater.inflate(mLayoutId, parent, false);
-			}
-			for (int i = 0; i < mTo.length; i++) {
-				TextView textView = (TextView) view.findViewById(mTo[i]);
-				String textData = (String) map.get(mFrom[i]);
-				try {
-					Double value = Double.parseDouble(textData);
-					if (value == 0) {
-						textView.setTextColor(firstColor);
-					} else {
-						textView.setTextColor(secondColor);
-					}
-				} catch (Exception e) {
-					textView.setTextColor(firstColor);
-				}
-				textView.setText(textData);
-			}
-			return view;
-		}
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            HashMap<String, Object> map = mData.get(position);
+            if (view == null) {
+                view = mInflater.inflate(mLayoutId, parent, false);
+            }
+            for (int i = 0; i < mTo.length; i++) {
+                TextView textView = (TextView) view.findViewById(mTo[i]);
+                String textData = (String) map.get(mFrom[i]);
+                try {
+                    Double value = Double.parseDouble(textData);
+                    if (value == 0) {
+                        textView.setTextColor(firstColor);
+                    } else {
+                        textView.setTextColor(secondColor);
+                    }
+                } catch (Exception e) {
+                    textView.setTextColor(firstColor);
+                }
+                textView.setText(textData);
+            }
+            return view;
+        }
 
-		@Override
-		public boolean isEnabled(int position) {
-			return false;
-		}
-	}
+        @Override
+        public boolean isEnabled(int position) {
+            return false;
+        }
+    }
 }
--- app/src/main/java/com/btc_e_assist/CustomExpandAdapter.java
+++ app/src/main/java/com/btc_e_assist/CustomExpandAdapter.java
@@ -1,9 +1,5 @@
 package com.btc_e_assist;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,163 +7,158 @@ import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class CustomExpandAdapter extends BaseExpandableListAdapter {
-	public ArrayList<Integer> checkData;
-	protected LayoutInflater mInflater;
-	protected List<? extends Map<String, ?>> mGroupData;
-	protected List<? extends List<? extends Map<String, ?>>> mChildData;
-	protected int mGroupLayout;
-	protected int mChildLayout;
-	protected int mImageId;
-	protected int mDividerId;
-	protected String[] mGroupFrom;
-	protected String[] mChildFrom;
-	protected String[] mAliases;
-	protected int[] mGroupTo;
-	protected int[] mChildTo;
-	protected int[] mAliasesDrawIds;
+    public ArrayList<Integer> checkData;
+    protected LayoutInflater mInflater;
+    protected List<? extends Map<String, ?>> mGroupData;
+    protected List<? extends List<? extends Map<String, ?>>> mChildData;
+    protected int mGroupLayout;
+    protected int mChildLayout;
+    protected int mImageId;
+    protected int mDividerId;
+    protected String[] mGroupFrom;
+    protected String[] mChildFrom;
+    protected String[] mAliases;
+    protected int[] mGroupTo;
+    protected int[] mChildTo;
+    protected int[] mAliasesDrawIds;
 
-	/**
-	 * Adapter for populating ExpandableListView, with checkBoxes and
-	 * aliases-Image function. groupData must have "alias" key with key from
-	 * aliases array
-	 * 
-	 * @param context
-	 * @param groupData
-	 * @param groupFrom
-	 * @param groupTo
-	 * @param childData
-	 * @param childFrom
-	 * @param childTo
-	 * @param groupLayout
-	 * @param childLayout
-	 * @param imageId
-	 *            id for insert DrawableLeft
-	 * @param aliases
-	 *            Array of aliases
-	 * @param aliasesDrawIds
-	 *            relative for aliases images
-	 */
-	public CustomExpandAdapter(Context context,
-			List<? extends Map<String, ?>> groupData, String[] groupFrom,
-			int[] groupTo,
-			List<? extends List<? extends Map<String, ?>>> childData,
-			String[] childFrom, int[] childTo, int groupLayout,
-			int childLayout, int imageId, int dividerId, String[] aliases,
-			int[] aliasesDrawIds) {
-		mInflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		mGroupData = groupData;
-		mChildData = childData;
-		mGroupLayout = groupLayout;
-		mChildLayout = childLayout;
-		mImageId = imageId;
-		mDividerId = dividerId;
-		mGroupFrom = groupFrom;
-		mChildFrom = childFrom;
-		mAliases = aliases;
-		mGroupTo = groupTo;
-		mChildTo = childTo;
-		mAliasesDrawIds = aliasesDrawIds;
-		checkData = new ArrayList<Integer>();
-	}
+    /**
+     * Adapter for populating ExpandableListView, with checkBoxes and
+     * aliases-Image function. groupData must have "alias" key with key from
+     * aliases array
+     *
+     * @param context
+     * @param groupData
+     * @param groupFrom
+     * @param groupTo
+     * @param childData
+     * @param childFrom
+     * @param childTo
+     * @param groupLayout
+     * @param childLayout
+     * @param imageId        id for insert DrawableLeft
+     * @param aliases        Array of aliases
+     * @param aliasesDrawIds relative for aliases images
+     */
+    public CustomExpandAdapter(Context context,
+                               List<? extends Map<String, ?>> groupData, String[] groupFrom,
+                               int[] groupTo,
+                               List<? extends List<? extends Map<String, ?>>> childData,
+                               String[] childFrom, int[] childTo, int groupLayout,
+                               int childLayout, int imageId, int dividerId, String[] aliases,
+                               int[] aliasesDrawIds) {
+        mInflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mGroupData = groupData;
+        mChildData = childData;
+        mGroupLayout = groupLayout;
+        mChildLayout = childLayout;
+        mImageId = imageId;
+        mDividerId = dividerId;
+        mGroupFrom = groupFrom;
+        mChildFrom = childFrom;
+        mAliases = aliases;
+        mGroupTo = groupTo;
+        mChildTo = childTo;
+        mAliasesDrawIds = aliasesDrawIds;
+        checkData = new ArrayList<Integer>();
+    }
 
-	@Override
-	public View getGroupView(int groupPosition, boolean isExpanded,
-			View convertView, ViewGroup parent) {
-		View view = convertView;
-		if (view == null) {
-			view = mInflater.inflate(mGroupLayout, parent, false);
-		}
-		Map<String, ?> map = mGroupData.get(groupPosition);
-		String currentAlias = (String) map.get("alias");
-		int count = mGroupTo.length;
-		for (int i = 0; i < count; i++) {
-			TextView v = (TextView) view.findViewById(mGroupTo[i]);
-			if (v != null) {
-				if (currentAlias != null) {
-					if (mGroupTo[i] == mImageId) {
-						int countAliases = mAliases.length;
-						for (int y = 0; y < countAliases; y++) {
-							if (mAliases[y].equals(currentAlias)) {
-								v.setCompoundDrawablesWithIntrinsicBounds(
-										mAliasesDrawIds[y], 0, 0, 0);
-								break;
-							}
-						}
-					}
-				}
-				v.setText((String) map.get(mGroupFrom[i]));
-			}
-		}
-		return view;
-	}
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = mInflater.inflate(mGroupLayout, parent, false);
+        }
+        Map<String, ?> map = mGroupData.get(groupPosition);
+        String currentAlias = (String) map.get("alias");
+        for (int i = 0; i < mGroupTo.length; i++) {
+            TextView v = (TextView) view.findViewById(mGroupTo[i]);
+            if (v != null && currentAlias != null && mGroupTo[i] == mImageId) {
+                for (int y = 0; y < mAliases.length; y++) {
+                    if (mAliases[y].equals(currentAlias)) {
+                        v.setCompoundDrawablesWithIntrinsicBounds(
+                                mAliasesDrawIds[y], 0, 0, 0);
+                        break;
+                    }
+                }
+            }
+            v.setText((String) map.get(mGroupFrom[i]));
+        }
+        return view;
+    }
 
-	@Override
-	public View getChildView(int groupPosition, int childPosition,
-			boolean isLastChild, View convertView, ViewGroup parent) {
-		View view = convertView;
-		if (view == null) {
-			view = mInflater.inflate(mChildLayout, parent, false);
-		}
-		if (isLastChild) {
-			if (groupPosition == getGroupCount() - 1) {
-				view.findViewById(mDividerId).setVisibility(View.GONE);
-			} else {
-				view.findViewById(mDividerId).setVisibility(View.VISIBLE);
-			}
-		} else {
-			view.findViewById(mDividerId).setVisibility(View.VISIBLE);
-		}
-		Map<String, ?> map = mChildData.get(groupPosition).get(childPosition);
-		int count = mChildTo.length;
-		for (int i = 0; i < count; i++) {
-			TextView v = (TextView) view.findViewById(mChildTo[i]);
-			if (v != null) {
-				v.setText((String) map.get(mChildFrom[i]));
-			}
-		}
-		return view;
-	}
+    @Override
+    public View getChildView(int groupPosition, int childPosition,
+                             boolean isLastChild, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null) {
+            view = mInflater.inflate(mChildLayout, parent, false);
+        }
+        if (isLastChild) {
+            if (groupPosition == getGroupCount() - 1) {
+                view.findViewById(mDividerId).setVisibility(View.GONE);
+            } else {
+                view.findViewById(mDividerId).setVisibility(View.VISIBLE);
+            }
+        } else {
+            view.findViewById(mDividerId).setVisibility(View.VISIBLE);
+        }
+        Map<String, ?> map = mChildData.get(groupPosition).get(childPosition);
+        int count = mChildTo.length;
+        for (int i = 0; i < count; i++) {
+            TextView v = (TextView) view.findViewById(mChildTo[i]);
+            if (v != null) {
+                v.setText((String) map.get(mChildFrom[i]));
+            }
+        }
+        return view;
+    }
 
-	@Override
-	public Object getChild(int groupPosition, int childPosition) {
-		return mChildData.get(groupPosition).get(childPosition);
-	}
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return mChildData.get(groupPosition).get(childPosition);
+    }
 
-	@Override
-	public int getChildrenCount(int groupPosition) {
-		return mChildData.get(groupPosition).size();
-	}
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return mChildData.get(groupPosition).size();
+    }
 
-	@Override
-	public Object getGroup(int groupPosition) {
-		return mGroupData.get(groupPosition);
-	}
+    @Override
+    public Object getGroup(int groupPosition) {
+        return mGroupData.get(groupPosition);
+    }
 
-	@Override
-	public int getGroupCount() {
-		return mGroupData.size();
-	}
+    @Override
+    public int getGroupCount() {
+        return mGroupData.size();
+    }
 
-	@Override
-	public long getGroupId(int groupPosition) {
-		return groupPosition;
-	}
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
 
-	@Override
-	public long getChildId(int groupPosition, int childPosition) {
-		return childPosition;
-	}
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
 
-	@Override
-	public boolean hasStableIds() {
-		return false;
-	}
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
 
-	@Override
-	public boolean isChildSelectable(int groupPosition, int childPosition) {
-		return true;
-	}
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
 
 }
